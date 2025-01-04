785c4a9946da6bcc2e2a9529d587da100e0a913a
Felipe Firmino Santos <felipefsantos.93@gmail.com>
2025-01-04 20:41:41 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 #Version without loop while.
 n = int(input("Write the number:\n"))
-print("The multiplication table of the number {} is...\n".format(n))
+print("The multiplication table of the number {} is...".format(n))
 print("{} x 0 = {}".format(n, n * 0))
 print("{} x 1 = {}".format(n, n * 1))
 print("{} x 2 = {}".format(n, n * 2))
@@ -15,9 +15,15 @@ print("{} x 10 = {}".format(n, n * 10))
 
 #Version with loop while.
 n = int(input("Write the number:\n"))
-print("The multiplication table of the number {} is...\n".format(n))
+print("The multiplication table of the number {} is...".format(n))
 count = 0
 while count <= 10:
     print("{} x {} = {}".format(n, count, n * count))
     count += 1
 
+#Version with list and loop for
+n = int(input("Write the number:\n"))
+print("The multiplication table of the number {} is...".format(n))
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for nl in list:
+    print("{} x {} = {}".format(n, nl, n * nl))
